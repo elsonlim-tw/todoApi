c7711dd68fadf7c12243a346718e720bc75170b5
elsonlim-tw <elsonlim@thoughtworks.com>
2020-05-18 15:20:57 +0800

    [elson] reduce test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,12 @@ module.exports = {
   preset: "@shelf/jest-mongodb",
   coverageThreshold: {
     global: {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90
-    }
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
   },
   setupFilesAfterEnv: ["./src/test-init/db-test-setup.js"],
-  testResultsProcessor: "jest-sonar-reporter"
+  testResultsProcessor: "jest-sonar-reporter",
 };
